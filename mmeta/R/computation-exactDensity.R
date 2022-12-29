a955d7e80d3d1f92e3eb1dc365c4ed247f6902b6
Jiajie Chen <chenjiajiemath@gmail.com>
2022-12-29 11:43:32 -0500

    Update computation-exactDensity.R

--- mmeta/R/computation-exactDensity.R
+++ mmeta/R/computation-exactDensity.R
@@ -4,14 +4,14 @@
 
 
 
-densityMeasureGrids <- function(parms_prior = parms_prior, data = data, 
+densityMeasureGrids <- function(parms_prior = parms_prior, data = data,
                                  model = model, measure = measure,
-                                from = from, to = to, 
+                                from = from, to = to,
                                 num_grids = num_grids){
-  
+
   density_grids = rep(0, num_grids * 1.5 )
   theta_grids = seq(from = from, to = to, by = (to - from)/(num_grids * 1.5 ))
-  
+
 
   for(i in 1: num_grids){
     density_grids[i] <- densityMeasure(theta = theta_grids[i],
@@ -22,14 +22,14 @@ densityMeasureGrids <- function(parms_prior = parms_prior, data = data,
                                      rho=parms_prior$rho,
                                      y1=data$y1,
                                      y2=data$y2,
-                                     n1=data$n1, 
-                                     n2=data$n2, 
+                                     n1=data$n1,
+                                     n2=data$n2,
                                      measure=measure,
                                      model=model
                                      )
-    
+
   }
-  
+
   density_grids <- density_grids[!is.na(density_grids)]
   length_density_grids <- length(density_grids)
   theta_grids <- theta_grids[!is.na(density_grids)]
@@ -55,7 +55,7 @@ densityMeasureGrids <- function(parms_prior = parms_prior, data = data,
 densityMeasure <- function(theta=theta,a1=a1,b1=b1,a2=a2,b2=b2,rho=rho,
                             y1=y1,y2=y2,n1=n1, n2=n2,
                             measure=measure,model=model) {
-  if (model=="Sarmanov"){ 
+  if (model=="Sarmanov"){
     density_measure <- densitySarmanovMeasure(theta=theta,a1=a1,b1=b1,a2=a2,b2=b2,rho=rho,
                                        y1=y1,y2=y2,n1=n1,n2=n2,
                                        measure=measure)
@@ -63,10 +63,10 @@ densityMeasure <- function(theta=theta,a1=a1,b1=b1,a2=a2,b2=b2,rho=rho,
   if (model=="Independent"){
     density_measure <- densityIndendentMeasure(theta=theta,a1=a1,b1=b1,a2=a2,b2=b2,
                                               y1=y1,y2=y2,n1=n1,n2=n2,
-                                              measure=measure) 
-      
+                                              measure=measure)
+
   }
-         
+
   return(density_measure)
 }
 
@@ -89,7 +89,7 @@ densityIndendentMeasure <- function(theta=theta,a1=a1,b1=b1,a2=a2,b2=b2,
   if (measure=="OR")  result <- densityIndendentOR(alpha1,beta1,alpha2,beta2,theta)
   if (measure=="RR")  result <- densityIndendentRR(alpha1,beta1,alpha2,beta2,theta)
   if (measure=="RD")  result <- NA
-  
+
   return(result)
 }
 
@@ -115,11 +115,11 @@ densityIndendentOR <- function(alpha1, beta1, alpha2, beta2, theta) {
       ((-1+alpha2)*log(theta)
                 -(lgamma(alpha1)+lgamma(beta1)+lgamma(alpha2)+lgamma(beta2)+lgamma(alpha1+beta1+alpha2+beta2))
                 +(lgamma(alpha1+beta1)+lgamma(alpha2+beta2)+lgamma(alpha1+alpha2)+lgamma(beta1+beta2))
-                +log(hypergeoFun(alpha2+beta2, alpha1+alpha2, alpha1+alpha2+beta1+beta2, 1-theta)))    
+                +log(hypergeoFun(alpha2+beta2, alpha1+alpha2, alpha1+alpha2+beta1+beta2, 1-theta)))
     )
     result <- exp(mylog)
   }
-  
+
   if(!is.nan(result))
     return(result)
   else return(NA)
@@ -132,7 +132,7 @@ densityIndendentOR <- function(alpha1, beta1, alpha2, beta2, theta) {
 ### Author:  Sheng Luo, Yong Chen, Xiao Su, Haitao Chu
 ### Data:    7/13/2012
 ###################################################################################
-densityIndendentRR <- function(alpha1, beta1, alpha2, beta2, theta){            
+densityIndendentRR <- function(alpha1, beta1, alpha2, beta2, theta){
   if(theta < 1){
     mylog <- suppressWarnings(
        ((-1+alpha2)*log(theta)
@@ -171,21 +171,21 @@ densityIndendentRR <- function(alpha1, beta1, alpha2, beta2, theta){
 #########################################################################################
 densitySarmanovMeasure <- function(theta=theta,a1=a1,b1=b1,a2=a2,b2=b2,rho=rho,
                      y1=y1,y2=y2,n1=n1,n2=n2,
-                     measure=measure) {                                                     
-  
+                     measure=measure) {
+
   alpha1 <- y1+a1; beta1 <- n1-y1+b1
   alpha2 <- y2+a2; beta2 <- n2-y2+b2
-  
-  myOmega <- omegaCal(y1, n1, y2, n2, a1, b1, a2, b2, rho)
+
+  myOmega <- omegaCal(y1, n1, y2, n2, a1, b1, a2, b2, rho) # weights in sec 2.2
   omega1 <- myOmega$omega1
   omega2 <- myOmega$omega2
   omega3 <- myOmega$omega3
   omega4 <- myOmega$omega4
   if (measure=="OR")
     result <- densitySarmanovOR(alpha1,beta1,alpha2,beta2,theta,omega1,omega2,omega3,omega4)
-  if (measure=="RR") 
+  if (measure=="RR")
     result <- densitySarmanovRR(alpha1,beta1,alpha2,beta2,theta,omega1,omega2,omega3,omega4)
-  if (measure=="RD")  
+  if (measure=="RD")
     result <- NA
   return(result)
 }
@@ -204,7 +204,7 @@ densitySarmanovOR <- function(alpha1,beta1,alpha2,beta2,theta,
   return(omega1*densityIndendentOR(alpha1,beta1,alpha2,beta2,theta)
          +omega2*densityIndendentOR(alpha1+1,beta1,alpha2,beta2,theta)
          +omega3*densityIndendentOR(alpha1,beta1,alpha2+1,beta2,theta)
-         +omega4*densityIndendentOR(alpha1+1,beta1,alpha2+1,beta2,theta))                 
+         +omega4*densityIndendentOR(alpha1+1,beta1,alpha2+1,beta2,theta))
 }
 
 ###################################################################################
@@ -217,8 +217,8 @@ densitySarmanovOR <- function(alpha1,beta1,alpha2,beta2,theta,
 densitySarmanovRR <- function(alpha1,beta1,alpha2,beta2,theta,
                         omega1,omega2,omega3,omega4) {
   results <- (omega1*densityIndendentRR(alpha1,beta1,alpha2,beta2,theta)
-              +omega2*densityIndendentRR(alpha1+1,beta1,alpha2,beta2,theta)            
-              +omega3*densityIndendentRR( alpha1,beta1,alpha2+1,beta2,theta)            
+              +omega2*densityIndendentRR(alpha1+1,beta1,alpha2,beta2,theta)
+              +omega3*densityIndendentRR( alpha1,beta1,alpha2+1,beta2,theta)
               +omega4*densityIndendentRR(alpha1+1,beta1,alpha2+1,beta2,theta))
   return(results)
 }
